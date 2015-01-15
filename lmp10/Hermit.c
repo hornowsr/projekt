49b06af4ad782fbf04ec137120323015388b53f0
Rafal Hornowski <hornowsr@len.iem.pw.edu.pl>
2015-01-15 16:35:45 +0100

    Napisanie 1 funkji+Poprawa Makefile+Poprawa Hermit.h

--- lmp10/Hermit.c
+++ lmp10/Hermit.c
@@ -1,62 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "makespl.h"
 #include "piv_ge_solver.h"
 
-double H(int step , double x)
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+double H( int stopien, double x )
 {
-	if(step == 0)
-		return 1;
-	if(step == 1)
-		return 2*x;
-	return 2*x*H(step-1,x)-2*(step-1)*H(step-2,x);
+        if( stopien == 0 ) return 1;
+        if( stopien == 1 ) return 2*x;
+        return 2*x * H( stopien-1, x) - 2*(stopien-1)* H(stopien-2,x);
+}
+ 
+double dla_y( int j , int max, double *z, double *c)
+{
 }
 
-double gdy_0(double a , int i , int k){}
-double gdy_1(double a , int i , int k){}
-double gdy_2(double a , int i , int k){}
-double gdy_3(double a , int i , int k){}
-double dla_y(double a){}
-
-void make_spl(points_t *pts, spline_t *spl)
+double gdy_0(double s, int j, int k , double *x)
 {
+	if(j == 0)
+		s = s+H(0,1);
+	else if(j == 1)
+		s = s+H(1,x[k]);
+	else if(j == 2)
+		s = s+H(2,x[k]);
+	else if(j == 3)
+		s = s+H(3,x[k]);
+
+	return s;
+}
+
+double gdy_1(double s, int j , int k , double *x)
+{
+}
+
+double gdy_2(double s, int j, int k, double *x)
+{
+}
+
+double gdy_3(double s, int j , int k , double *x)
+{
+}
+void
+make_spl(points_t * pts, spline_t *spl)
+{
+
+	matrix_t       *eqs= NULL;
+	double         *x = pts->x;
+	double         *y = pts->y;
+	int		i, j, k, max=pts->n;
+	int 		m = 4;
+	double 		s=0;
+	eqs = make_matrix(m, m+1);
 	
-
-	int ILOSC = pts->n;
-	matrix_t *eqs = NULL;
-	double *x = pts->x;  //wartosci x-ow
-	double *y = pts->y;  //wartosci y-ow
-	int i , j , k;
-	int m = 4;  //rozmier 
-	double s = 0; //przechowywanie wzoru
-
-	eqs = make_matrix(m,m+1);
-
-
-	for(i = 0 ; i < m ; i++)
+	for (j = 0; j < m; j++) 
+	{
+		
+		for (i = 0; i < m; i++)
 		{
-				for(j = 0 ; j < m ; j++)
-				{
-					s = 0;
-						for(k = 0 ; k < ILOSC ; k++)
-						{
-							if(j == 0)
-								gdy_0(s , i , k);
-							else if(j == 1)
-								gdy_1(s , i , k);
-							else if(j == 2)
-								gdy_2(s , i , k);
-							else
-								gdy_3(s , i , k);
-						}
-
-					add_to_entry_matrix(eqs, i, j , s);
-				}
 			s=0;
-			dla_y(s , ILOSC);
-
-			add_to_entry_matrix(eqs , j , m , s);
+			for (k = 0; k < max; k++)
+			{	
+				if(i==0)
+				s=gdy_0(s,j,k,x);
+				else if (i==1)
+				s=gdy_1(s,j,k,x);
+				else if (i==2)
+				s=gdy_2(s,j,k,x);
+				else if (i==3)
+				s=gdy_3(s,j,k,x);
+			}
+			add_to_entry_matrix(eqs, j ,i,s);
 		}
+		s=0;
+		s=dla_y(j,max,x,y);
+			add_to_entry_matrix(eqs, j, m,s );
+	}
+	if(piv_ge_solver(eqs))
+	{
+		spl->n = 0;
+		return;
+	}
 
-}				
+}
 
--- lmp10/Hermit.h
+++ lmp10/Hermit.h
@@ -1,20 +1,20 @@
 #ifndef _HERMIT_H_
 #define _HERMIT_H
 
-#include "piv_de_solver.h"
-#include "makespl.h"
 
 double H(int step , double x);
 
-double gdy_0(double a , int i , int k);
+//void sprawdz(spline_t *a , matrix_t *b);
 
-double gdy_1(double a , int i , int k);
+double gdy_0(double a , int i , int k , double *x);
 
-double gdy_2(double a , int i , int k);
+double gdy_1(double *a , int i , int k , double *x);
 
-double gdy_3(double a , int i , int k);
+double gdy_2(double a , int i , int k , double *x);
 
-double dla_y(double a);
+double gdy_3(double a , int i , int k , double*x);
+
+double dla_y( int i , int ILE , double *x , double *y);
 
 
 #endif
